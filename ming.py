e04bdf241e167e1636be15aba761ac4cb91d905a
samuripper <samu@linuxasylum.net>
2017-03-23 21:53:58 +0100

    scrapers completed

--- ming.py
+++ ming.py
@@ -12,18 +12,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+import codecs
 
 
 login_url = 'https://mijn.ing.nl/internetbankieren/SesamLoginServlet'
-redirect_page = 'https://bankieren.mijn.ing.nl/particulier/betalen/index'
-
-click_id = 'advancedSearch'
-
-datada_id = 'dateRangeMin-input'
-dataa_id = 'dateRangeMax-input'
-submit_id = 'form-submit-btnA'
-more_id = 'showMore'
-
 
 config = configparser.ConfigParser()
 config.read('credential')
@@ -34,9 +26,31 @@ driver = webdriver.Chrome()
 driver.set_window_size(1920, 1080)
 driver.get(login_url)
 user = driver.find_element_by_xpath("//input[@type='text']")
-pasd = driver.find_element_by_xpath("//input[@type='password']")
 user.send_keys(username)
+passwordlabel = driver.find_element_by_xpath('//*[@id="wachtwoord"]/label')
+pasd = driver.find_element_by_xpath("//*[@id='{}']".format(passwordlabel.get_attribute("for")))
 pasd.send_keys(password)
 login_button = driver.find_element_by_xpath("//button[@class='submit']")
 login_button.click()
 time.sleep(2)
+
+adv_button = driver.find_element_by_xpath('//*[@id="adv-search-txt"]')
+adv_button.click()
+cal_min = driver.find_element_by_xpath('//*[@id="dateRangeMin-input"]')
+cal_max = driver.find_element_by_xpath('//*[@id="dateRangeMax-input"]')
+
+cal_min.send_keys("01-03-2016")
+cal_max.send_keys("01-03-2017")
+time.sleep(2)
+search_button = driver.find_element_by_xpath('//*[@id="form-submit-btnA"]')
+search_button.click()
+time.sleep(2)
+more = driver.find_element_by_xpath('//*[@id="showMore"]')
+while more.is_displayed():
+    more.click()
+    time.sleep(0.5)
+time.sleep(3)
+html_source = driver.page_source
+fw = codecs.open("cc", 'w', encoding='utf8')
+fw.write(html_source)
+fw.close()
